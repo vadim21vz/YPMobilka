b7e8e2ae0cd2d2b02456498cce6e65fb9956fe8c
vzzz <111954001+vadim21vz@users.noreply.github.com>
2025-06-02 08:55:12 +0300

    Sprint-1 commit

---
+++ app/src/main/java/com/example/mdkyp/Onboard2.java
@@ -0,0 +1,30 @@
+package com.example.mdkyp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Onboard2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void onClick (View v){
+        Intent intent = new Intent(this, Onboard3.class);
+        startActivity(intent);
+    }
+}
